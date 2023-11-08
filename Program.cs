505a832ab0d458d8b01b7cc068c735759b4f24e8
pheck <pheck@live.com>
2023-11-08 13:56:50 -0800

    Updated code from both repos

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("==========================================================");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Git Submodule Demo");
+            Console.ResetColor();
+
+            List<verCheckData> datas = new();
+            datas.Add(new verCheckData() { Name="EC", Hash= Guid.NewGuid().ToString() });
+            datas.Add(new verCheckData() { Name = "OPS", Hash = Guid.NewGuid().ToString() });
+
+
+            foreach (var data in datas)
+            {
+                Console.WriteLine($"   > Name: {data.Name}, Hash: {data.Hash}");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Goodbye!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("==========================================================");
+            Console.ResetColor();
+
         }
     }
 }
